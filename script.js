bbe572e9ca4abcad3bef01662b8e626a9af6dc9a
IvoBraatz <braatzivo@hotmail.com>
2024-11-19 21:43:42 -0300

    minify

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-// Garante que o scroll funcione no parallax
-document.addEventListener('scroll', () => {
-    const sections = document.querySelectorAll('.parallax-container');
-
-    sections.forEach(section => {
-        const building = section.querySelector('.building');
-        const sectionRect = section.getBoundingClientRect();
-        const windowHeight = window.innerHeight;
-
-        // Calcula o progresso dentro do viewport
-        const progress = Math.min(Math.max((windowHeight - sectionRect.top) / windowHeight, 0), 1);
-
-        // Define o movimento baseado no progresso
-        const translateY = (1 - progress) * 100; // Progressão mais linear e suave
-        building.style.transform = `translateY(${translateY}%)`;
-    });
-});
+document.addEventListener("scroll",(()=>{document.querySelectorAll(".parallax-container").forEach((e=>{const t=e.querySelector(".building"),n=e.getBoundingClientRect(),r=window.innerHeight,a=100*(1-Math.min(Math.max((r-n.top)/r,0),1));t.style.transform=`translateY(${a}%)`}))}));
